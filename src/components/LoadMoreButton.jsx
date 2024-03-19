76606b6700a9b90e3ddd352b7345826bcfef5139
im_abramon <shelopugin.vlad@yandex.ru>
2024-03-19 13:07:18 +0300

    LoadMoreButton стилизация

--- src/components/LoadMoreButton.jsx
+++ src/components/LoadMoreButton.jsx
@@ -1,10 +1,24 @@
 import React from 'react';
 import styled from 'styled-components';
 
-const Button = styled.button``;
+const Button = styled.button`
+  box-shadow: none;
+  border: none;
+  font-family: 'Open Sans';
+  font-size: 12px;
+  line-height: 20px;
+  letter-spacing: 0.5px;
+  font-weight: 500;
+  width: 502px;
+  height: 50px;
+  background-color: #2196f3;
+  color: white;
+  border-radius: 5px;
+  text-transform: uppercase;
+`;
 
 const LoadMoreButton = () => {
-  return <Button>Загрузить Еще</Button>;
+  return <Button>Показать еще 5 билетов</Button>;
 };
 
 export default LoadMoreButton;
