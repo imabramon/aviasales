b09b17fc2382f30f5d815f875e9cfe611afdb908
im_abramon <shelopugin.vlad@yandex.ru>
2024-03-19 11:06:57 +0300

    Стилизация тикет лист

--- src/components/TiketList.jsx
+++ src/components/TiketList.jsx
@@ -2,11 +2,32 @@ import React from 'react';
 import Tiket from './Tiket';
 import styled from 'styled-components';
 
-const Container = styled.div``;
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 20px;
+`;
 
 const TiketList = (props) => {
   return (
     <Container>
+      <Tiket
+        price="13400"
+        flightsData={[
+          {
+            cites: 'MOW – HKT',
+            times: '10:45 – 08:00',
+            travelTime: '21ч 15м',
+            transfers: ['HKG', 'JNB'],
+          },
+          {
+            cites: 'MOW – HKT',
+            times: '10:45 – 08:00',
+            travelTime: '21ч 15м',
+            transfers: ['HKG', 'JNB'],
+          },
+        ]}
+      />
       <Tiket
         price="13400"
         flightsData={[
