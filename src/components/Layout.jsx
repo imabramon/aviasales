c3149103a88d3d08bc5fd50eafb545e3553b69d7
im_abramon <shelopugin.vlad@yandex.ru>
2024-03-19 13:51:25 +0300

    Стилизация Layout

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,10 +1,30 @@
 import React from 'react';
 import styled from 'styled-components';
 
-const Container = styled.div``;
-const Header = styled.div``;
-const Content = styled.section``;
-const Aside = styled.aside``;
+const Container = styled.div`
+  display: grid;
+  grid-template-areas: 'header header' 'aside content';
+  width: 756px;
+  margin-left: auto;
+  margin-right: auto;
+  column-gap: 20px;
+`;
+const Header = styled.div`
+  grid-area: header;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 160px;
+`;
+const Content = styled.section`
+  grid-area: content;
+  display: flex;
+  flex-direction: column;
+  gap: 20px;
+`;
+const Aside = styled.aside`
+  grid-area: aside;
+`;
 
 const Layout = (props) => {
   const { header, children, aside } = props;
