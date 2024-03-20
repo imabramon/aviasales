a96f18857ea607e758f1fb0e012c2658858ffdd1
im_abramon <shelopugin.vlad@yandex.ru>
2024-03-20 10:43:22 +0300

    Написал экшены

--- src/store/actions.js
+++ src/store/actions.js
@@ -1,15 +1,42 @@
+import { makeTiketData } from '../utils/tikets';
 import { ActionTypes } from './actionTypes';
 
-export const loadMore = () => ({
-  type: ActionTypes.loadMore,
+export const load = () => {
+  const tikets = Array.from({ length: 5 })
+    .fill(0)
+    .map((_, index) => makeTiketData(index));
+
+  return {
+    type: ActionTypes.load,
+    payload: {
+      tikets,
+    },
+  };
+};
+
+export const changeSort = (sort) => ({
+  type: ActionTypes.changeSort,
+  payload: {
+    sort,
+  },
 });
 
-export const applyFilter = (filter) => ({
-  type: ActionTypes.applyFilter,
-  payload: { filter },
+export const changeFilter = (filter) => ({
+  type: ActionTypes.changeFilter,
+  payload: {
+    filter,
+  },
 });
 
-export const applyTransferFilter = (filter) => ({
-  type: ActionTypes.applyTransferFilter,
-  payload: { filter },
-});
+export const loadMore = () => {
+  const tikets = Array.from({ length: 5 })
+    .fill(0)
+    .map((_, index) => makeTiketData(index));
+
+  return {
+    type: ActionTypes.loadMore,
+    payload: {
+      tikets,
+    },
+  };
+};
