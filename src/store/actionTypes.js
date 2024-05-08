88fc47ea8e0b6d928e748daf36959bcf48d5274e
im_abramon <shelopugin.vlad@yandex.ru>
2024-05-08 14:51:45 +0300

    чиста последняя

--- src/store/actionTypes.js
+++ src/store/actionTypes.js
@@ -1,8 +1,7 @@
 export const ActionTypes = {
-  load: 'load',
   changeSort: 'changeSort',
   changeFilter: 'changeFilter',
   loadMore: 'loadMore',
   viewMore: 'viewMore',
-  setButtonState: 'setButtonState'
+  setButtonState: 'setButtonState',
 };
